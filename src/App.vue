8f80dfc8b83f96fafd5dc397970fa57636c0aa7c
P4L <cornelius.weling@gmail.com>
2020-04-19 18:27:15 +0400

    move setting panel from App.vue to MainScreen.vue

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,5 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated class="glossy">
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          @click="leftDrawerOpen = !leftDrawerOpen"
-          aria-label="Menu"
-          icon="menu"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-2"
-    >
-      <q-list>
-        <q-item-label header>Settings</q-item-label>
-        <q-item clickable>
-          <q-item-section avatar>
-            <toggle-button @change="mlChange" v-model="useML" />
-          </q-item-section>
-          <q-item-section>
-            Распознавание лиц {{ useML }}
-          </q-item-section>
-        </q-item>
-        <q-item clickable>
-          <q-item-section avatar>
-            <toggle-button @change="mlChange" v-model="usePID" />
-          </q-item-section>
-          <q-item-section>
-            ПИД {{ usePID }}
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
       <MainScreen />
     </q-page-container>
@@ -54,28 +8,19 @@
 
 <script>
 import MainScreen from './components/MainScreen.vue'
-import { ToggleButton } from 'vue-js-toggle-button'
 
 export default {
   name: 'LayoutDefault',
 
   components: {
-    MainScreen,
-    ToggleButton
+    MainScreen
   },
 
   data () {
     return {
       leftDrawerOpen: false,
-      useML: false,
-      usePID: false
     }
-  },
-  methods: [
-    function mlChange() {
-
-    }
-  ]
+  }
 }
 </script>
 
--- src/components/MainScreen.vue
+++ src/components/MainScreen.vue
@@ -1,14 +1,76 @@
 <template>
-  <q-page class="flex flex-center">
-    MainScreen
-  </q-page>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="glossy">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+          icon="menu"
+        />
+
+        <q-toolbar-title>Панель управления автомоделью</q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-2">
+      <q-list>
+        <q-item-label header>Settings</q-item-label>
+        <q-item clickable @click="mlChange()">
+          <q-item-section avatar>
+            <toggle-button v-model="useML" />
+          </q-item-section>
+          <q-item-section>Распознавание лиц</q-item-section>
+        </q-item>
+        <q-item clickable @click="pidChange">
+          <q-item-section avatar>
+            <toggle-button v-model="usePID" />
+          </q-item-section>
+          <q-item-section>ПИД</q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+    <q-page class="flex flex-center">
+      <img alt="Quasar logo" src="../assets/logo.png" />
+    </q-page>
+  </q-layout>
 </template>
+
+<script>
+import { ToggleButton } from "vue-js-toggle-button";
+
+export default {
+  name: "MainScreen",
+
+  components: {
+    ToggleButton
+  },
+
+  data() {
+    return {
+      leftDrawerOpen: false,
+      useML: false,
+      usePID: false
+    };
+  },
+  methods: [
+    function created() {
+      alert(11);
+    },
+    function mlChange() {
+      alert(123);
+      if (this.useML == false) this.useML = true;
+      else this.useML = false;
+    },
+    function pidChange() {
+      if (this.usePID == false) this.usePID = true;
+      else this.usePID = false;
+    }
+  ]
+};
+</script>
 
 <style>
 </style>
-
-<script>
-export default {
-  name: 'MainScreen'
-}
-</script>
