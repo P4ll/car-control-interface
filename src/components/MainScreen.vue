3d56295a46f68ae9b0c4b94e11e4a936f2f5303f
P4L <cornelius.weling@gmail.com>
2020-04-19 22:29:56 +0400

    add timeout function

--- src/components/MainScreen.vue
+++ src/components/MainScreen.vue
@@ -1,84 +1,89 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated class="glossy">
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          @click="leftDrawerOpen = !leftDrawerOpen"
-          aria-label="Menu"
-          icon="menu"
-        />
+    <q-layout view="lHh Lpr lFf">
+        <q-header elevated class="glossy">
+            <q-toolbar>
+                <q-btn
+                    flat
+                    dense
+                    round
+                    @click="leftDrawerOpen = !leftDrawerOpen"
+                    aria-label="Menu"
+                    icon="menu"
+                />
 
-        <q-toolbar-title>Панель управления автомоделью</q-toolbar-title>
-      </q-toolbar>
-    </q-header>
+                <q-toolbar-title>Панель управления автомоделью</q-toolbar-title>
+            </q-toolbar>
+        </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-2">
-      <q-list>
-        <q-item-label header>Settings</q-item-label>
-        <q-item clickable>
-          <q-item-section avatar>
-            <toggle-button v-model="useML" />
-          </q-item-section>
-          <q-item-section>Распознавание лиц {{useML}}</q-item-section>
-        </q-item>
-        <q-item clickable>
-          <q-item-section avatar>
-            <toggle-button v-model="usePID" />
-          </q-item-section>
-          <q-item-section>ПИД</q-item-section>
-        </q-item>
-      </q-list>
-    </q-drawer>
-    <q-page class="flex flex-center">
-      <img alt="Quasar logo" src="../assets/logo.png" />
-      <div id="pic"></div>
-    </q-page>
-  </q-layout>
+        <q-drawer
+            v-model="leftDrawerOpen"
+            show-if-above
+            bordered
+            content-class="bg-grey-2"
+        >
+            <q-list>
+                <q-item-label header>Settings</q-item-label>
+                <q-item clickable>
+                    <q-item-section avatar>
+                        <toggle-button v-model="useML" />
+                    </q-item-section>
+                    <q-item-section
+                        >Распознавание лиц {{ useML }}</q-item-section
+                    >
+                </q-item>
+                <q-item clickable>
+                    <q-item-section avatar>
+                        <toggle-button v-model="usePID" />
+                    </q-item-section>
+                    <q-item-section>ПИД</q-item-section>
+                </q-item>
+            </q-list>
+        </q-drawer>
+        <q-page class="flex flex-center">
+            <img alt="Quasar logo" src="../assets/logo.png" @load="test()" />
+            <div id="pic"></div>
+        </q-page>
+    </q-layout>
 </template>
 
 <script>
 import { ToggleButton } from "vue-js-toggle-button";
 
 export default {
-  name: "MainScreen",
+    name: "MainScreen",
 
-  components: {
-    ToggleButton
-  },
-
-  data() {
-    return {
-      leftDrawerOpen: false,
-      useML: false,
-      usePID: false
-    };
-  },
-
-  mounted() {
-  },
-
-  methods: {
-    test() {
-        
+    components: {
+        ToggleButton,
     },
-    created() {
-      alert(11);
+
+    data() {
+        return {
+            leftDrawerOpen: false,
+            useML: false,
+            usePID: false,
+            intervalId: null
+        };
     },
-    mlChange() {
-      alert(123);
-      if (this.useML == false) this.useML = true;
-      else this.useML = false;
+
+    mounted() {},
+
+    methods: {
+        test() {
+            this.intervalId = setInterval(this.test2, 0);
+        },
+        test2() {
+            alert('interval');
+        },
+        mlChange() {
+            if (this.useML == false) this.useML = true;
+            else this.useML = false;
+        },
+        pidChange() {
+            if (this.usePID == false) this.usePID = true;
+            else this.usePID = false;
+        },
     },
-    pidChange() {
-      if (this.usePID == false) this.usePID = true;
-      else this.usePID = false;
-    }
-  }
 };
 </script>
 
-<style>
-</style>
+<style></style>
