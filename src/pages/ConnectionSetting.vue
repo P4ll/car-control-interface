978a7dfc5b2dd7ade02945ec3467ffb6ab564140
a-ponomarev-truemachine <52834764+a-ponomarev-truemachine@users.noreply.github.com>
2020-04-21 14:55:18 +0200

    Input
    add two input

--- src/pages/ConnectionSetting.vue
+++ src/pages/ConnectionSetting.vue
@@ -1,7 +1,23 @@
 <template>
   <q-page class="flex flex-center">
     this is a connection setting page
+    <q-input filled v-model="text" placeholder="192.168.0.X" hint="Ip" :dense="dense">
+        <template v-slot:append>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+          </q-avatar>
+        </template>
+      </q-input>
+      <br>
+      <q-input filled v-model="text" placeholder="8765" hint="Port" :dense="dense">
+        <template v-slot:append>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+          </q-avatar>
+        </template>
+      </q-input>
     <q-btn label="Управлять!" to="/control"/>
+    <q-btn label="Проверка!" v-on:click="greet"/>
   </q-page>
 </template>
 
